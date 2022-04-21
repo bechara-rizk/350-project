f78693e30b3dc19b62d34353436ca918376c5ef6
Bechara Rizk <becharaerizk@yahoo.com>
2022-04-21 21:30:04 +0300

    file

--- file_over_tcp/file_client_tcp.py
+++ file_over_tcp/file_client_tcp.py
@@ -2,20 +2,18 @@ from math import ceil
 from socket import *
 import time
 import os
-# import tqdm
 
 
 serverName = "127.0.0.1"
 serverPort = 12000
-chunk_size = 512
+chunk_size = 2048
 sep="\n"
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
 
-file_name = input("Enter the name of the file: ")  # THE FILE TO BE SENT SHOULD BE IN THE SAME DIRECTORY AS THIS FILE
+file_name = "test.txt"#input("Enter the name of the file: ")  # THE FILE TO BE SENT SHOULD BE IN THE SAME DIRECTORY AS THIS FILE
 file_size = os.path.getsize(file_name)
 file_type = file_name.split(".")[1]
-
 hello_msg = file_name+sep+file_type+sep+str(file_size)
 # print(hello_msg)
 clientSocket.sendto(hello_msg.encode(), (serverName, serverPort)) # SENDING HELLO MESSAGE WHICH INCLUDES FILE NAME, TYPE AND SIZE
@@ -31,6 +29,8 @@ print("nb of chunks: ",nb_chunks)  # number of chunks to be sent
 i = 1
 # progress = tqdm.tqdm(range(file_size), f"Sending {file_name}", unit="B", unit_scale=True, unit_divisor=1024) # progress bar
 with open(file_name, "rb") as file:
+
+    time.sleep(0.1)
     while True:
         chunk = file.read(chunk_size)
         # progress.update(len(chunk))             # update progress bar
--- file_over_tcp/file_server_tcp.py
+++ file_over_tcp/file_server_tcp.py
@@ -1,8 +1,7 @@
 from socket import *
-import tqdm  # to download this library: in cmd: pip install tqdm
 
 serverPort = 12000
-chunk_size = 512
+chunk_size = 2048
 
 sep="\n"
 chunks=[]
@@ -16,15 +15,19 @@ connectionSocket, addr= serverSocket.accept()
 hello_rcvd = connectionSocket.recv(chunk_size)
 hello_rcvd = hello_rcvd.decode()
 hello_rcvd = hello_rcvd.split(sep)
-new_file = "new_"+hello_rcvd[0].split(".")[0]+"."+hello_rcvd[1]
+#new_file = "new_"+hello_rcvd[0].split(".")[0]+"."+hello_rcvd[1]
+new_file = "new_test.txt"
 print("new file name: ",new_file)
 
 file = open(new_file, "wb")
+# print(hello_rcvd)
 progress = tqdm.tqdm(range(int(hello_rcvd[2])), f"Receiving {hello_rcvd[0]}", unit="B", unit_scale=True, unit_divisor=1024)  # optional - requires tqdm
 while True:
     bytes_read = connectionSocket.recv(chunk_size)
+    # print(bytes_read)
     chunks.append(bytes_read)
     if (len(bytes_read) < chunk_size):
+        progress.update(len(bytes_read))
         progress.close()                               # optional - requires tqdm
         break
     else:
